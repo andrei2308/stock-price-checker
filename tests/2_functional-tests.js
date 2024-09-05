791ae20260f80ae2595e2bc0ea7b624fa829a7c0
andrei2308 <chitoiu.andrei2@yahoo.com>
2024-09-05 18:23:18 +0300

    adding functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,81 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+test('Viewing on stock: GET request to /api/stock-prices/',function(done){
+    chai.request(server)
+    .get('/api/stock-prices')
+    .query({stock:"AAPL"})
+    .end(function(err,res){
+        assert.equal(res.status,200);
+        assert.isObject(res.body,'Response should be an object!!')
+        assert.property(res.body,'stockData','Response should have stockData')
+        assert.property(res.body.stockData,"symbol","Stock data should have a symbol")
+        assert.property(res.body.stockData,'price','Stock data should have a price')
+        assert.equal(res.body.stockData.symbol,"AAPL","Stock symbol should be AAPL")
+        done();
+    });
+});
+test('Viewing one stock and liking it: GET request to /api/stock-prices',function(done){
+    chai.request(server)
+    .get('/api/stock-prices')
+    .query({stock:"AAPL",like:true})
+    .end(function(err,res){
+    assert.equal(res.status,200)
+    assert.isObject(res.body,'Response should be an object !!');
+    assert.property(res.body,'stockData','Response should have stockData')
+    assert.property(res.body.stockData,'Stock data should have a symbol');
+    assert.property(res.body.stockData,'price','Stock data should have a price');
+    assert.equal(res.body.stockData.symbol,'AAPL','Stock symbol should be AAPL');
+    assert.isAtLeast(res.body.stockData.likes,1,'Likes should be at least 1')
+    done();
+    })
+})
+test('Viewing the same stock and liking it again: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL', like: true })  
+      .end(function(err, res) {
+        assert.equal(res.status, 200);  
+        assert.isObject(res.body, 'Response should be an object');
+        assert.property(res.body, 'stockData', 'Response should have stockData');
+        assert.property(res.body.stockData, 'symbol', 'Stock data should have a symbol');
+        assert.property(res.body.stockData, 'price', 'Stock data should have a price');
+        assert.property(res.body.stockData, 'likes', 'Stock data should have a likes property');
+        assert.equal(res.body.stockData.symbol, 'GOOG', 'Stock symbol should be GOOG');
+        assert.isAtLeast(res.body.stockData.likes, 1, 'Likes should still be at least 1');
+        done();
+      });
+  });
+  test('Viewing two stocks: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'AAPL'] })
+      .end(function(err, res) {
+        assert.equal(res.status, 200);  
+        assert.isArray(res.body.stockData, 'Response should be an array');
+        assert.lengthOf(res.body.stockData, 2, 'Array should have two elements');
+        assert.property(res.body.stockData[0], 'symbol', 'First stock should have a symbol');
+        assert.property(res.body.stockData[0], 'price', 'First stock should have a price');
+        assert.property(res.body.stockData[1], 'symbol', 'Second stock should have a symbol');
+        assert.property(res.body.stockData[1], 'price', 'Second stock should have a price');
+        done();
+      });
+  });
+  test('Viewing two stocks and liking them: GET request to /api/stock-prices/', function(done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'AAPL'], like: true }) 
+      .end(function(err, res) {
+        assert.equal(res.status, 200); 
+        assert.isArray(res.body.stockData, 'Response should be an array');
+        assert.lengthOf(res.body.stockData, 2, 'Array should have two elements');
+        assert.property(res.body.stockData[0], 'symbol', 'First stock should have a symbol');
+        assert.property(res.body.stockData[0], 'price', 'First stock should have a price');
+        assert.property(res.body.stockData[0], 'likes', 'First stock should have likes');
+        assert.property(res.body.stockData[1], 'symbol', 'Second stock should have a symbol');
+        assert.property(res.body.stockData[1], 'price', 'Second stock should have a price');
+        assert.property(res.body.stockData[1], 'likes', 'Second stock should have likes');
+        done();
+      });
+  });
 });
