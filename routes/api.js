46cb3d2dbb5b3629502a3dee15099d9df65686a0
andrei2308 <chitoiu.andrei2@yahoo.com>
2024-09-06 17:31:34 +0300

    handle get requests

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,14 @@
 'use strict';
 
+const axios = require('axios'); // Assuming axios is used for fetching stock data
+let stockLikes = {};  // To store likes for each stock (this should be in a database in a real app)
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
-    });
-    
+    .get(async function (req, res) {
+      let stock = req.query.stock; 
+      let like = req.query.like === 'true';  
+      }
+    );
 };
